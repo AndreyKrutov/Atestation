677f0343bf97007a47e4227beae1a2dff5c2718d
AndreyKrutov <99074047+AndreyKrutov@users.noreply.github.com>
2022-03-10 12:21:59 +0300

    Add files via upload

---
+++ SchoolSchedule/ArithmeticMean.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolSchedule
+{
+    public class ArithmeticMean
+    {
+        public double MinAVG(string[] marks)
+        {
+            double i = 0;
+            return i;
+        }
+    }
+}
---
+++ SchoolSchedule/Estimation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolSchedule
+{
+    public class Estimation
+    {
+        public List<string> GetMarks(DateTime now, List<string> student)
+        {
+            List<string> people = new List<string>();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < student.Count; i++)
+            {
+                int[] numbers = { rnd.Next(2, 6), rnd.Next(2, 6), rnd.Next(2, 6), rnd.Next(2, 6), rnd.Next(2, 6), rnd.Next(2, 6), rnd.Next(2, 6), rnd.Next(2, 6), rnd.Next(2, 6), rnd.Next(2, 6) };
+                int g = 0;
+                for(int y = 0; y < 9; y++)
+                {
+                    g = g + numbers[y];
+                }
+                float z = g / numbers.Length;
+                people.Add(student[i] + " - " + numbers[0] + ", " + numbers[1] + ", " + numbers[2] + ", " + numbers[3] + ", " + numbers[4] + ", " + numbers[5] + ", " + numbers[6] + ", " + numbers[7] + ", " + numbers[8] + ", " + numbers[9] + " = " + z);
+            }
+
+            return people;
+        }
+    }
+}
